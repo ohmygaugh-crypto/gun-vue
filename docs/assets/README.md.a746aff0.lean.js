79811b0282a37aac330b3bb4ecff00164b5c62d7
davay <fond@fondfrukt.ru>
2022-04-16 14:59:30 +0300

    deploy

--- docs/assets/README.md.a746aff0.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as s,o as a,a as t}from"./app.a9be7f43.js";const g='{"title":"Vitepress SSR web-site with @gun-vue components","description":"","frontmatter":{},"headers":[],"relativePath":"README.md"}',p={},o=t("",10),e=[o];function c(u,l,i,r,k,d){return a(),s("div",null,e)}var v=n(p,[["render",c]]);export{g as __pageData,v as default};
